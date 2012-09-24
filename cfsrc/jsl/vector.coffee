99c284a088d4732e1253eb5a84b683c85c37248a
ablonevn <tungbuivn@gmail.com>
2012-09-24 12:37:15 +0700

    a

--- cfsrc/jsl/vector.coffee
+++ cfsrc/jsl/vector.coffee
@@ -1,7 +1,45 @@
 class Jsl.Vector
-  constructor:
-    return
-create=->
-  new Vector(arguments)
+  dx:0
+  dy:0
+  constructor:->
+    me=this
+    # console.log me
+    # window.a=me
+    Jsl.Vector.prototype.init.apply(me,arguments)
+    # if (typeof dx isnt 'undefined')
+      # Jsl.Vector.init.apply(this,arguments)
+    # if arguments.length==2
+      # me.dx=arguments[0]
+      # me.dy=arguments[1]
+    
+    me
+  setVector:(@dx,@dy)->
+    @
+  init:->
+    me=this
+    switch arguments.length
+      when 2 
+        me.dx=arguments[0]
+        me.dy=arguments[1]
+        break
+      
+      when 4
+        xfrom=arguments[0]
+        yfrom=arguments[1]
+        xto=arguments[2]
+        yto=arguments[3]
+        me.setVector(xto-xfrom,yto-yfrom)
+        break
+    a
+    
+
+vector=
+  create:->
+    console.log(arguments)
+    a=new Jsl.Vector()
+    a.init.apply(a,arguments)
+    a
+
+
 Jsl.apply Jsl,
-  vector.create
+  vector:vector
--- jsl/vector.js
+++ jsl/vector.js
@@ -1,11 +1,60 @@
 (function() {
+  var vector;
 
   Jsl.Vector = (function() {
 
-    function Vector() {}
+    Vector.prototype.dx = 0;
+
+    Vector.prototype.dy = 0;
+
+    function Vector() {
+      var me;
+      me = this;
+      Jsl.Vector.prototype.init.apply(me, arguments);
+      me;
+    }
+
+    Vector.prototype.setVector = function(dx, dy) {
+      this.dx = dx;
+      this.dy = dy;
+      return this;
+    };
+
+    Vector.prototype.init = function() {
+      var me, xfrom, xto, yfrom, yto;
+      me = this;
+      switch (arguments.length) {
+        case 2:
+          me.dx = arguments[0];
+          me.dy = arguments[1];
+          break;
+        case 4:
+          xfrom = arguments[0];
+          yfrom = arguments[1];
+          xto = arguments[2];
+          yto = arguments[3];
+          me.setVector(xto - xfrom, yto - yfrom);
+          break;
+      }
+      return a;
+    };
 
     return Vector;
 
   })();
+
+  vector = {
+    create: function() {
+      var a;
+      console.log(arguments);
+      a = new Jsl.Vector();
+      a.init.apply(a, arguments);
+      return a;
+    }
+  };
+
+  Jsl.apply(Jsl, {
+    vector: vector
+  });
 
 }).call(this);
