57a3aacc05ef7414a84da67a4a4db77ef91a9e34
ablonevn <tungbuivn@gmail.com>
2012-09-24 16:29:36 +0700

    a

--- cfsrc/jsl/matrix.coffee
+++ cfsrc/jsl/matrix.coffee
@@ -1,36 +1,44 @@
 class Jsl.Matrix
   m:[]
   constructor:(@m=[])->
-
+    @
   print:(data=@m)->
     for v in data
       console.log v    
-    
+  size:(data=@m)->
+    col:data[0].length
+    row:data.length
   rows:(i,data=@m)->
     data[i]
   cols:(i,data=@m)->
     rs=[]
     for row in data
       rs.push(row[i])
-    rs      
+    rs
+  multiplySum:(a,b)->
+    sum=0
+    for i in [0..a.length-1]
+      sum+=a[i]*b[i]
+    sum
   multiply:(a)->
+    targetSize={
+      row:@.size().row
+      col:@.size(a).col
+    }
+    # console.log(targetSize)
     me=this
-    
     rs=[]
     # debugger;
-    for j in [0..@m.length-1]
+    for j in [0..targetSize.row-1]
       row=@rows(j)
-      
       r=[]
-      
-      for k in [0..row.length-1]
+      for k in [0..targetSize.col-1]
         col= @cols(k,a)
-        sum=0
-        for l in [0..row.length-1]
-          sum+=row[l]*col[l]
+        sum=@multiplySum(row,col)
         r.push(sum)
       rs.push(r)
-    @print(rs)
+    # @print(rs)
+    @
     
   x:->
     @multiply.apply(@,arguments)
--- jsl/matrix.js
+++ jsl/matrix.js
@@ -6,6 +6,7 @@
 
     function Matrix(m) {
       this.m = m != null ? m : [];
+      this;
     }
 
     Matrix.prototype.print = function(data) {
@@ -17,6 +18,14 @@
         _results.push(console.log(v));
       }
       return _results;
+    };
+
+    Matrix.prototype.size = function(data) {
+      if (data == null) data = this.m;
+      return {
+        col: data[0].length,
+        row: data.length
+      };
     };
 
     Matrix.prototype.rows = function(i, data) {
@@ -35,24 +44,34 @@
       return rs;
     };
 
+    Matrix.prototype.multiplySum = function(a, b) {
+      var i, sum, _ref;
+      sum = 0;
+      for (i = 0, _ref = a.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+        sum += a[i] * b[i];
+      }
+      return sum;
+    };
+
     Matrix.prototype.multiply = function(a) {
-      var col, j, k, l, me, r, row, rs, sum, _ref, _ref2, _ref3;
+      var col, j, k, me, r, row, rs, sum, targetSize, _ref, _ref2;
+      targetSize = {
+        row: this.size().row,
+        col: this.size(a).col
+      };
       me = this;
       rs = [];
-      for (j = 0, _ref = this.m.length - 1; 0 <= _ref ? j <= _ref : j >= _ref; 0 <= _ref ? j++ : j--) {
+      for (j = 0, _ref = targetSize.row - 1; 0 <= _ref ? j <= _ref : j >= _ref; 0 <= _ref ? j++ : j--) {
         row = this.rows(j);
         r = [];
-        for (k = 0, _ref2 = row.length - 1; 0 <= _ref2 ? k <= _ref2 : k >= _ref2; 0 <= _ref2 ? k++ : k--) {
+        for (k = 0, _ref2 = targetSize.col - 1; 0 <= _ref2 ? k <= _ref2 : k >= _ref2; 0 <= _ref2 ? k++ : k--) {
           col = this.cols(k, a);
-          sum = 0;
-          for (l = 0, _ref3 = row.length - 1; 0 <= _ref3 ? l <= _ref3 : l >= _ref3; 0 <= _ref3 ? l++ : l--) {
-            sum += row[l] * col[l];
-          }
+          sum = this.multiplySum(row, col);
           r.push(sum);
         }
         rs.push(r);
       }
-      return this.print(rs);
+      return this;
     };
 
     Matrix.prototype.x = function() {
